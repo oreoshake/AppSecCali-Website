06ef8cba6aa5a23ae6989767d766151a3d3e5403
Neil Matatall <neilm@twitter.com>
2013-05-09 19:35:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecruitLy::Application.config.secret_key_base = '99044eff6e68ef54d52095624fa3aa239ffd03249de85114c77d10e21cc32416e625a47886a347eb50cd1d82ffec5e3ad2a4bcb5d93483f024b1c3c6cfc97be2'
