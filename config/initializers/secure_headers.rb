bcc5010e58208225a6d4ecad4c157348cdc9ae9c
Neil Matatall <neilm@twitter.com>
2013-06-23 15:16:14 -0700

    asdf

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,7 +1,7 @@
 SecureHeaders::Configuration.configure do |config|
   config.csp = {
   	:default_src => 'self',
-  	:script_src => 'self https://platform.twitter.com',
+  	:script_src => 'self https://platform.twitter.com http://platform.twitter.com',
   	:style_src => 'self inline',
   	:img_src => 'https://si0.twimg.com',
   	:frame_src => 'https://www.youtube.com http://www.youtube.com http://platform.twitter.com https://platform.twitter.com https://maps.google.com http://maps.google.com',
