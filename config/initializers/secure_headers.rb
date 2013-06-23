bd45d445e1e477fe594e682be1fc5e69ed4fd84e
Neil Matatall <neilm@twitter.com>
2013-06-23 14:58:39 -0700

    asdf

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,7 +4,7 @@ SecureHeaders::Configuration.configure do |config|
   	:script_src => 'self https://platform.twitter.com',
   	:style_src => 'self inline',
   	:img_src => 'https://si0.twimg.com',
-  	:frame_src => 'https://www.youtube.com https://maps.google.com',
+  	:frame_src => 'https://www.youtube.com http://www.youtube.com http://platform.twitter.com https://maps.google.com http://maps.google.com',
   	:enforce => true
   }
 end
