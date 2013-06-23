aba5c8cf4854c64992e1916a1cffc35b2a7a92f6
Neil Matatall <neilm@twitter.com>
2013-06-23 15:01:57 -0700

    redir

--- app/controllers/beta_requests_controller.rb
+++ app/controllers/beta_requests_controller.rb
@@ -4,7 +4,7 @@ class BetaRequestsController < ApplicationController
 
     if beta_request.save
       flash[:notice] = "Thanks for signing up!"
-      redirect_to root
+      redirect_to root_path
     else
       flash[:error] = beta_request.errors.full_messages.to_sentence
       redirect_to :back
