b284e1b0f995b5dd61ad9a701f29e2a216286ee8
Neil Matatall <neilm@twitter.com>
2013-08-31 16:36:21 -0700

    a bunch of shit

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,10 +1,10 @@
 SecureHeaders::Configuration.configure do |config|
   config.csp = {
   	:default_src => 'self',
-  	:script_src => 'self https://platform.twitter.com http://platform.twitter.com',
-  	:style_src => 'self inline',
+  	:script_src => 'self https://platform.twitter.com http://platform.twitter.com https://*.twimg.com',
+  	:style_src => 'self inline https://platform.twitter.com',
   	:img_src => 'https://si0.twimg.com',
-  	:frame_src => 'https://www.youtube.com http://www.youtube.com http://platform.twitter.com https://platform.twitter.com https://maps.google.com http://maps.google.com',
+  	:frame_src => 'https://www.youtube.com http://www.youtube.com http://*.twitter.com https://*.twitter.com https://maps.google.com http://maps.google.com',
   	:enforce => true
   }
 end
