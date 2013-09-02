fc321854b0ec54113ac6aca074d26028e3c5966f
Neil Matatall <neilm@twitter.com>
2013-09-02 00:30:19 -0700

    Buncha changes

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,5 @@
 class Speaker < ActiveRecord::Base
+	validates :email, :allow_blank => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+	validates :twitter_handle, :allow_blank => true, :format => { :with => /@?\w+/ }
+	validates :image_url, :allow_blank => true, :format => { :with => /\Ahttps?:\/\// }
 end
