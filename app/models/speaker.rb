fec3f3c3e4b839f5aa1e3cb1a418806bbf4ba1f5
Neil Matatall <neilm@twitter.com>
2013-09-02 22:36:29 -0700

    https image urls only

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,7 +1,7 @@
 class Speaker < ActiveRecord::Base
 	validates :email, :allow_blank => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 	validates :twitter_handle, :allow_blank => true, :format => { :with => /\A@?\w+\z/ }
-	validates :image_url, :allow_blank => true, :format => { :with => /\Ahttps?:\/\/.*\z/ }
+	validates :image_url, :allow_blank => true, :format => { :with => /\Ahttps:\/\/.*\z/ }
 	validates :website, :allow_blank => true, :format => { :with => /\Ahttps?:\/\/.*\z/ }
 	validates_presence_of :abstract, :email, :name
 end
--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -3,7 +3,7 @@ SecureHeaders::Configuration.configure do |config|
   	:default_src => 'self',
   	:script_src => 'self http://www.google-analytics.com https://www.google-analytics.com https://platform.twitter.com http://platform.twitter.com https://*.twimg.com http://*.twimg.com',
   	:style_src => 'self inline https://platform.twitter.com http://platform.twitter.com',
-  	:img_src => 'self https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
+  	:img_src => 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
   	:frame_src => 'https://www.youtube.com http://www.youtube.com http://*.twitter.com https://*.twitter.com https://twitter.com http://twitter.com https://maps.google.com http://maps.google.com',
     :report_uri => '/csp_report',
   	:enforce => true
