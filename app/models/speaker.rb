b427e49f3cda07565f40ded9626b0f5139fa4b14
oreoshake <neil@matatall.com>
2013-09-08 11:24:05 -0700

    sunday cleanup

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,7 +1,7 @@
 class Speaker < ActiveRecord::Base
 	validates :email, email: true, uniqueness: {message: "You have already submitted to the CFP. Thanks!"}, presence: true
-	validates :twitter_handle, :allow_blank => true, :format => { :with => /\A@?\w+\z/ }
-	validates :image_url, :allow_blank => true, :format => { :with => /\Ahttps:\/\/.*\z/ }
-	validates :website, :allow_blank => true, :format => { :with => /\Ahttps?:\/\/.*\z/ }
-	validates_presence_of :abstract, :email, :name
+	validates :twitter_handle, allow_blank: true, format: { with: /\A@?\w+\z/ }
+	validates :image_url, allow_blank: true, format: { with: /\Ahttps:\/\/.*\z/ }
+	validates :website, allow_blank: true, format: { with: /\Ahttps?:\/\/.*\z/ }
+	validates_presence_of :abstract, :name
 end
