4f9b05eb1ea26eabca173f4881228412585782ce
oreoshake <neil@matatall.com>
2013-09-07 21:13:51 -0700

    loosen csp

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,10 +1,10 @@
 SecureHeaders::Configuration.configure do |config|
   config.csp = {
-  	:default_src => 'self',
+  	:default_src => 'self https:',
   	:script_src => 'self http://www.google-analytics.com https://www.google-analytics.com https://platform.twitter.com http://platform.twitter.com https://*.twimg.com http://*.twimg.com',
   	:style_src => 'self inline https://platform.twitter.com http://platform.twitter.com',
   	:img_src => 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
-  	:frame_src => 'https://cloudfront.net https://www.youtube.com http://www.youtube.com http://*.twitter.com https://*.twitter.com https://twitter.com http://twitter.com https://maps.google.com http://maps.google.com',
+  	:frame_src => 'https: https://cloudfront.net https://www.youtube.com http://www.youtube.com http://*.twitter.com https://*.twitter.com https://twitter.com http://twitter.com https://maps.google.com http://maps.google.com',
     :report_uri => '/csp_report',
   	:enforce => true
   }
