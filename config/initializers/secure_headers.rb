4cfc250dcb511aeaac91e90588be4ff7647bd404
oreoshake <neil@matatall.com>
2013-09-15 00:02:21 -0700

    tighten csp

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,12 +1,16 @@
 SecureHeaders::Configuration.configure do |config|
-  config.hsts = {:max_age => 20.years.to_i, :include_subdomains => true}
+  config.hsts = { 
+    max_age: 20.years.to_i, 
+    include_subdomains: true
+  }
   config.csp = {
-  	:default_src => 'self https:',
-  	:script_src => 'self http://www.google-analytics.com https://www.google-analytics.com https://platform.twitter.com http://platform.twitter.com https://*.twimg.com http://*.twimg.com',
-  	:style_src => 'self inline https://platform.twitter.com http://platform.twitter.com',
-  	:img_src => 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
-  	:frame_src => 'https: https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com http://twitter.com',
-    :report_uri => '/csp_report',
-  	:enforce => true
+  	default_src: 'self',
+  	script_src: 'self https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com http://*.twimg.com',
+  	style_src: 'self inline https://platform.twitter.com',
+  	img_src: 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
+  	frame_src: 'https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com',
+    report_uri: '/csp_report',
+    disable_chrome_extension: true,
+  	enforce: true
   }
 end
