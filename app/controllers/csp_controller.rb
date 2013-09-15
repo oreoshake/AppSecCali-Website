b211a63461b20f067599eaa5c636ca46949db510
oreoshake <neil@matatall.com>
2013-09-14 23:14:42 -0700

    fix csp endpoint

--- app/controllers/csp_controller.rb
+++ app/controllers/csp_controller.rb
@@ -1,7 +1,7 @@
 class CspController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 	def csp_report
-		Rails.logger.error(params['csp-report'].merge('user-agent', request.env["HTTP_USER_AGENT"]))
+		Rails.logger.error((params['csp-report']||{}).merge('user-agent' => request.env["HTTP_USER_AGENT"]))
 		head :ok
 	end
 end
