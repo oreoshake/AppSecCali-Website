acbd39205b5e71af8429f4e8cda63f37e6dcce6c
Neil Matatall <neilm@twitter.com>
2013-09-15 17:51:46 -0700

    csp update

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,11 +1,11 @@
 SecureHeaders::Configuration.configure do |config|
-  config.hsts = { 
-    max_age: 20.years.to_i, 
+  config.hsts = {
+    max_age: 20.years.to_i,
     include_subdomains: true
   }
   config.csp = {
   	default_src: 'self',
-  	script_src: 'self https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com http://*.twimg.com',
+  	script_src: 'self https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com https://www.google.com',
   	style_src: 'self inline https://platform.twitter.com',
   	img_src: 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
   	frame_src: 'https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com',
