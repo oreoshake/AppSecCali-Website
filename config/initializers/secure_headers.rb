91488136f717deb3344be41b973c366ed6ce2e12
Neil Matatall <neilm@twitter.com>
2013-09-15 17:54:37 -0700

    wouldn't work coz of inline script anyways

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -5,7 +5,7 @@ SecureHeaders::Configuration.configure do |config|
   }
   config.csp = {
   	default_src: 'self',
-  	script_src: 'self https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com https://www.google.com https://ssl.gstatic.com',
+  	script_src: 'self https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com',
   	style_src: 'self inline https://platform.twitter.com',
   	img_src: 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
   	frame_src: 'https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com',
