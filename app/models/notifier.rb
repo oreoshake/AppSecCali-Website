e2250b804600725cee85af7866177a982b50168a
Neil Matatall <neilm@twitter.com>
2013-09-20 10:01:22 -0700

    Right email address

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,12 +3,12 @@ class Notifier < ActionMailer::Base
   sendgrid_enable   :subscriptiontrack
   sendgrid_category :use_subject_lines
 
-  default :from => "owasp-cali-planners@owasp.org"
+  default :from => "owasp-cali-planners@appseccalifornia.org"
 
   def cfp_submission(speaker)
-    sendgrid_recipients ["owasp-cali-planners@owasp.org"]
+    sendgrid_recipients ["owasp-cali-planners@appseccalifornia.org"]
     @speaker = speaker
-    mail( to: "owasp-cali-planners@owasp.org",
+    mail( to: "owasp-cali-planners@appseccalifornia.org",
       subject: 'Your CFP submission has been received.' )
   end
 end
