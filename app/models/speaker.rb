3898bc5639a6a32bcf9bf1c2e0fa58fd5201c4fd
Neil Matatall <neilm@twitter.com>
2013-10-10 10:51:00 -0700

    less restrictive

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,7 +1,4 @@
 class Speaker < ActiveRecord::Base
 	validates :email, email: true, presence: true
-	validates :twitter_handle, allow_blank: true, format: { with: /\A@?\w+\z/ }
-	validates :image_url, allow_blank: true, format: { with: /\Ahttps:\/\/.*\z/ }
-	validates :website, allow_blank: true, format: { with: /\Ahttps?:\/\/.*\z/ }
 	validates_presence_of :abstract, :name
 end
