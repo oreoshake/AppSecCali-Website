afb23ebb1e995f95a88d3a5d5d3bc0fa81b10f08
oreoshake <neil@matatall.com>
2013-12-07 08:33:11 -0800

    csp config

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -5,10 +5,10 @@ SecureHeaders::Configuration.configure do |config|
   }
   config.csp = {
   	default_src: 'self',
-  	script_src: 'self https://ssl.google-analytics.com http://owaspappseccalifornia2014.sched.org https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com',
+  	script_src: 'self https://syndication.twitter.com https://ssl.google-analytics.com http://owaspappseccalifornia2014.sched.org https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com',
   	style_src: 'self inline https://platform.twitter.com',
   	img_src: 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
-  	frame_src: 'https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com',
+  	frame_src: 'http://owaspappseccalifornia2014.sched.org https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com',
     report_uri: '/csp_report',
     disable_chrome_extension: true,
   	enforce: false
