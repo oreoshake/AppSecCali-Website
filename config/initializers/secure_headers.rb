ee13143c09825e32c3be669566fa6f4e36b23ea8
Neil Matatall <neil.matatall@gmail.com>
2013-12-14 12:06:14 -0800

    sched dot org embed

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,10 +6,10 @@ SecureHeaders::Configuration.configure do |config|
   config.csp = {
   	default_src: 'self',
   	script_src: 'self https://syndication.twitter.com https://ssl.google-analytics.com http://owaspappseccalifornia2014.sched.org https://www.google-analytics.com https://platform.twitter.com https://*.twimg.com',
-  	style_src: 'self inline https://platform.twitter.com',
-  	img_src: 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com',
+  	style_src: 'self inline https://platform.twitter.com http://cdn.schd.ws',
+  	img_src: 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com http://cdn.schd.ws',
   	frame_src: 'http://owaspappseccalifornia2014.sched.org https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com',
-    report_uri: '/csp_report',
+    report_uri: 'https://twitter.com/scribes/csp_report',
     disable_chrome_extension: true,
   	enforce: false
   }
