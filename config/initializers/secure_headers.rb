acb43a2421883dad65e148859e1a681fdae83503
Neil Matatall <neil.matatall@gmail.com>
2013-12-14 13:25:16 -0800

    csp

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -9,9 +9,9 @@ SecureHeaders::Configuration.configure do |config|
   	style_src: 'self inline https://platform.twitter.com http://cdn.schd.ws',
   	img_src: 'self https: https://si0.twimg.com http://www.google-analytics.com https://www.google-analytics.com http://cdn.schd.ws',
   	frame_src: 'http://owaspappseccalifornia2014.sched.org https://www.ssllabs.com https://cloudfront.net http://*.twitter.com https://*.twitter.com https://twitter.com',
-    report_uri: 'https://twitter.com/scribes/csp_report',
+    report_uri: '/csp_report',
     disable_chrome_extension: true,
-  	enforce: false
+  	enforce: true
   }
 
 
